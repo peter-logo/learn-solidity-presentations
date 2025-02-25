3ed33ee2ee79bdc814bf471c25e2036dc3375f3e
Peter <peterlogo@krownpay.com>
2025-02-25 11:06:50 -0500

    Update Example.sol

--- 1a-value-types/examples/0-example-value-types/src/Example.sol
+++ 1a-value-types/examples/0-example-value-types/src/Example.sol
@@ -7,6 +7,8 @@ contract Example {
     uint8 a = 255; // 0 -> 255
     uint256 b = 22; // alias: uint
 
+    constructor() {}
+
     int8 c = 127; // -128 -> 127
     int256 d = -55; // alias: int256
 
